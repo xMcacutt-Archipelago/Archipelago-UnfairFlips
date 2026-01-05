23340b002fca5f684784de980b587a44f5946f20
Noa Aarts <noa@voorwaarts.nl>
2026-01-05 02:09:48 +0100

    increase MAX_HEADS_CHANCE to 95% to allow 47+ heads to be in logic

--- worlds/unfair_flips/data.py
+++ worlds/unfair_flips/data.py
@@ -2,6 +2,6 @@ MAX_HEADS = 50
 JUNK_FACTOR = 0.3 # amount of items placements reserved for junk for shop upgrades
 MIN_COMBO = 1.2
 MAX_COMBO = 3.5
-MAX_HEADS_CHANCE = 0.9
+MAX_HEADS_CHANCE = 0.95
 PRACTICALITY_THRESHOLD = 0.0075
 SHOP_LAYERS_PER_GATE_INDEX = 2
